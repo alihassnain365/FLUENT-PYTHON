2a52a756c27fd9fe436bfd1f342b0da9fffc5536
Ali Hassnain <ali.hassnain0365@gmail.com>
2026-07-21 12:13:34 +0500

    sending *arguments

--- CHAPTER_2_ARRAY_OF_SEQUENCES/genepxs.py
+++ CHAPTER_2_ARRAY_OF_SEQUENCES/genepxs.py
@@ -1,12 +1,27 @@
-colors = ['red','blue']
-sizes = ['s','m','l']
+# colors = ['red','blue']
+# sizes = ['s','m','l']
 
-shirts_pairs = ((color,size) for color in colors 
-                            for size in sizes)
-print(shirts_pairs)
-for pair in shirts_pairs:
-    print(pair)
+# shirts_pairs = ((color,size) for color in colors 
+#                             for size in sizes)
+# print(shirts_pairs)
+# for pair in shirts_pairs:
+#     print(pair)
 
-for pair in shirts_pairs:
-    print(pair)
+# for pair in shirts_pairs:
+#     print(pair)
+
+
+
+def add(x, y):
+    """Added two numbers"""
+    return x + y
+
+t = (1,2)
+#add(t) # it would give us error as the add expects two arguments
+add(*t) # it tells take every element from the t, pass it as an argument seperately
+
+"""What if t has more than the required arguemnts"""
+
+v = (1,2,3)
+add(*v) # it would also give an error
 
