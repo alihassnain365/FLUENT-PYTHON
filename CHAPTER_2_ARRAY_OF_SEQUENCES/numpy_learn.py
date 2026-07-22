b34df681a7b760c92916a36bd9b49fb263b08616
Ali Hassnain <ali.hassnain0365@gmail.com>
2026-07-22 20:45:15 +0500

    introdunction to numpy

---
+++ CHAPTER_2_ARRAY_OF_SEQUENCES/numpy_learn.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+a = np.arange(12)
+print(a)
+
+# print(type(a))
+print(a.shape)
+
+a.shape = 3,4
+print(a)
+
+print(a.shape)
+
+# print(a[2])
+
+# print(a[2,2])
+print(a[:,2])
+
+print(a)
+a.transpose
+print(a)
+
+
+
