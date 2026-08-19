fff6e963326e0946b6093d796de49794540e70dc
Ali Hassnain <ali.hassnain0365@gmail.com>
2026-08-19 08:18:19 +0500

    decorators two questions solved out of ten

--- CHAPTER_09_DECORATORS_AND_CLOSURES/02_decorator.py
+++ CHAPTER_09_DECORATORS_AND_CLOSURES/02_decorator.py
@@ -1,14 +1,41 @@
-def deco(func):
-    print("I am deco")
-    def wrapper():
-        func()
+"""Exercise 1 — Your First Decorator"""
+# def deco(func):
+#     def wrapper():
+#         print("Function is about to run.")
+#         func()
+#         print("Function is run is finished")
+#     return wrapper
+
+# @deco
+# def greet():
+#     print("Hello, Ali")
+
+
+
+# greet()
+
+
+"""Exercise 2 — Decorators With Function Arguments"""
+
+def logger(func):
+    def wrapper(*args, **kwargs):
+        print(f"Arguments({args})")
+        result = func(*args,**kwargs)
+        print(f"Result = {result}")
     return wrapper
 
+@logger
+def add(a,b):
+    return a+b
 
-@deco
-def cutie():
-    print("I am cutie pie")
+@logger
+def multiply(a,b):
+    return a*b
+
+add(2,3)
+multiply(5,4)
 
 
-print('Program is starting from here')
+
+
 
